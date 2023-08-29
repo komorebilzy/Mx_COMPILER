8adf7e465a7d9b8f0da24cbc6c3f1ecca9483a63
komorebilz <2110429490@qq.com>
2023-08-30 00:08:42 +0800

    remove some files

--- patch2.sh
+++
@@ -1,5 +0,0 @@
-echo 'Host github.com
-	HostName github.com
-	ProxyCommand ProxyCommand ssh jump@hk2.wxzheng.com -i ~/.ssh/id_hknode_jump -W %h:%p' > ~/.ssh/config
-
-ssh -T git@github.com
--- test.bash
+++
@@ -1,7 +0,0 @@
-clang-15 -S test.ll --target=riscv32-unknown-elf -o test.s.source -m32
-
-sed 's/@plt$//g' test.s.source > test.s
-
-rm -r -f test.s.source
-
-ravel --input-file=test.in --output-file=test.out test.s builtin.s
