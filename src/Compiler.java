a95036794d15a84a5219405ef3202dad392fbca0
komorebilz <2110429490@qq.com>
2023-08-30 00:29:45 +0800

    delete some files and try oj

--- src/Compiler.java
+++ src/Compiler.java
@@ -40,46 +40,34 @@ public class Compiler {
         }
     }
 
-    public static void compile(InputStream input, PrintStream IROutput, PrintStream AsmOutput) throws Exception{
-        GlobalScope globalScope= new GlobalScope();
+    public static void compile(InputStream input, PrintStream IROutput, PrintStream AsmOutput) throws Exception {
+        GlobalScope globalScope = new GlobalScope();
 
-        MxLexer lexer=new MxLexer(CharStreams.fromStream(input));
+        MxLexer lexer = new MxLexer(CharStreams.fromStream(input));
         lexer.removeErrorListeners();
         lexer.addErrorListener(new MxErrorListener());
 
-        MxParser parser=new MxParser(new CommonTokenStream(lexer));
+        MxParser parser = new MxParser(new CommonTokenStream(lexer));
         parser.removeErrorListeners();
         parser.addErrorListener(new MxErrorListener());
-        MxParser.ProgramContext parserTreeRoot=parser.program();
+        MxParser.ProgramContext parserTreeRoot = parser.program();
 
-        ASTBuilder astbuilder=new ASTBuilder();
-        RootNode ASTRoot=(RootNode) astbuilder.visit(parserTreeRoot);
+        ASTBuilder astbuilder = new ASTBuilder();
+        RootNode ASTRoot = (RootNode) astbuilder.visit(parserTreeRoot);
 
         new SymbolCollector(globalScope).visit(ASTRoot);
         new SemanticChecker(globalScope).visit(ASTRoot);
 
-        IRProgram rootIR=new IRProgram();
-        new IRBuilder(rootIR,globalScope).visit(ASTRoot);
+        IRProgram rootIR = new IRProgram();
+        new IRBuilder(rootIR, globalScope).visit(ASTRoot);
         new IRPrinter(IROutput).visit(rootIR);
 
         AsmModule asmModule = new AsmModule();
-        try{
-            new InsSelector(asmModule).visit(rootIR);
-        }
-        catch (Error err){
-            throw new codgenError("hhh",null);
-        }
-        try {
-            new RegAlloca().visit(asmModule);
-        }
-        catch (Error err){
-            throw new codgenError("yyy",null);
-        }
-        try {
-            new AsmPrinter(AsmOutput).print(asmModule);
-        }
-        catch (Error err){
-            throw new codgenError("xxx",null);
-        }
+
+        new InsSelector(asmModule).visit(rootIR);
+
+        new RegAlloca().visit(asmModule);
+
+        new AsmPrinter(AsmOutput).print(asmModule);
     }
 }
